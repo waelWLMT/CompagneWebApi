522c4fa36a37d63f5d3271440d460a5b3af72f52
unknown <wael.mrabet.dev@gmail.com>
2025-04-21 20:34:04 +0200

    add reporting controller

---
+++ WebApi/Controllers/ReportController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using BL.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GenerateClientReport")]
+
+        public IActionResult GenerateClientReport()
+        {
+            return null;
+        }
+
+
+    }
+}
